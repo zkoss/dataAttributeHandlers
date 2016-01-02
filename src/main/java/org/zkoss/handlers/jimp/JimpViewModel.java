4fb7f17b23c7cc5df348d7161b0c349af767939c
Mathieu Piette <mathieu.piette@gmail.com>
2016-01-03 00:04:25 +0100

    Using client-binding to publish the processed image to the ViewModel.

--- src/main/java/org/zkoss/handlers/jimp/JimpViewModel.java
+++ src/main/java/org/zkoss/handlers/jimp/JimpViewModel.java
@@ -1,13 +1,30 @@
 package org.zkoss.handlers.jimp;
 
+import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.annotation.ToServerCommand;
+
 /**
  * @author Mathieu Piette
  */
+@ToServerCommand({ "camanCalculationFinished" })
 public class JimpViewModel {
 
 	private int brightness = 3;
 	private String color = "#B000B5"; // It's funny because boobs
 	private int contrast = 1;
+	
+	/**
+	 * The base-64 encoded jpeg data of the processed image.
+	 */
+	private String jpegData;
+
+	@Command
+	@NotifyChange("jpegData")
+	public void camanCalculationFinished(@BindingParam("data") String jpegData) {
+		setJpegData(jpegData);
+	}
 
 	public int getBrightness() {
 		return brightness;
@@ -21,6 +38,10 @@ public class JimpViewModel {
 		return contrast;
 	}
 
+	public String getJpegData() {
+		return jpegData;
+	}
+
 	public void setBrightness(int brightness) {
 		this.brightness = brightness;
 	}
@@ -31,6 +52,10 @@ public class JimpViewModel {
 
 	public void setContrast(int contrast) {
 		this.contrast = contrast;
+	}
+
+	public void setJpegData(String jpegData) {
+		this.jpegData = jpegData;
 	}
 
 }
--- src/main/webapp/jimp/scripts/data-jimp.js
+++ src/main/webapp/jimp/scripts/data-jimp.js
@@ -1,9 +1,15 @@
 function (wgt, dataValue) {
+	var binder = zkbind.$('$myWindow');
+	
 	Caman("#" + wgt.uuid, wgt.getSrc(), function () {
 		this
 			.contrast(dataValue.contrast)
 			.brightness(dataValue.brightness)
 			.colorize(dataValue.color, 20)
-			.render();
+			.render(function() {
+				var jpegData = document.getElementById(wgt.uuid).toDataURL("image/jpeg");
+				
+				binder.command('camanCalculationFinished', {data: jpegData});
+			});
 	});
 }
